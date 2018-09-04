9586fe6ce4552e677cc12a41de7d8c84a6d3e499
kaegjhs <keaza_cc@hotmail.com>
2018-09-05 05:51:17 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,12 @@
 <?php
-echo "Hello LINE BOT";
+//echo "Hello LINE BOT";
+require "vendor/autoload.php";
+$access_token = 'c7FPNNIpO7lYkSVeqjRvQDAsvczv5G70+BDFDCXXVLTazFLNIcD0WwCzoOgW5yJ63MLueTPcfSt9JpgZtekQP87g/zaKxic9TaNp9yK3ab1kPPuQP9ZWI9NlsX9YkXZWn/yfda5jAUDiAr+1Qu6m9gdB04t89/1O/w1cDnyilFU=';
+$channelSecret = '82fdafeaf4d24ddf26c1e52026be9586';
+$idPush = 'U92e74f8ce3595165ba396dbef155629a'
+$httpClient = new \LINE\LINEBot\HTTPClient\CurlHTTPClient($access_token);
+$bot = new \LINE\LINEBot($httpClient, ['channelSecret' => $channelSecret]);
+$textMessageBuilder = new \LINE\LINEBot\MessageBuilder\TextMessageBuilder('สวัสดีจ้า');
+$response = $bot->pushMessage($idPush, $textMessageBuilder);
+
+echo $response->getHTTPStatus() . ' ' . $response->getRawBody();
